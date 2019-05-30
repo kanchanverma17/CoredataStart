fa466f25c63b5cdf1a4c9a4be67702048886930f
Kanchan <kanchanverma17@users.noreply.github.com>
2019-05-30 17:09:12 +0530

    first commit to git local

---
+++ CoredataStart/customLayout.swift
@@ -0,0 +1,15 @@
+//
+//  customLayout.swift
+//  CoredataStart
+//
+//  Created by Kanchan on 30/05/19.
+//  Copyright © 2019 Kanchan. All rights reserved.
+//
+
+import UIKit
+
+class customLayout: UICollectionViewLayout
+{
+    
+    
+}
---
+++ CoredataStart/parentSession.swift
@@ -0,0 +1,16 @@
+//
+//  parentSession.swift
+//  CoredataStart
+//
+//  Created by Kanchan on 30/05/19.
+//  Copyright © 2019 Kanchan. All rights reserved.
+//
+
+import Foundation
+
+let apikey = "89932206"
+
+class masterSession: URLSession
+{
+    
+}
